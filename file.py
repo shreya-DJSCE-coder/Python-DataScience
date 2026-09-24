b2c6b657f74dc45a02ebb24e9966594e81ff9379
Shreya Yadav <shreyayadav9619573842@gmail.com>
2026-09-24 21:49:44 +0530

    file io is completed with list comprehension

--- file.py
+++ file.py
@@ -62,17 +62,61 @@
 # finally:  #if we want the some code that should be always execute irrespective of exception thrown or not 
 #     print("Our program was done")  
 
-li=[]
-for i in range(6): #0 to 5
-    li.append(i*i)
+# li=[]
+# for i in range(6): #0 to 5
+#     li.append(i*i)
 
-print(li)    
+# print(li)    
 
-sq=[i*i for i in range(6)]
-print(sq)
+# sq=[i*i for i in range(6)]
+# print(sq)
 
-ad=[i+2 for i in range(9)]
-print(ad)
+# ad=[i+2 for i in range(9)]
+# print(ad)
+
+# sqp=[i*i for i in range(6) if(i%2!=0)]
+# print(sqp)
+
+# lip=[-2,-4,3,5,2,-1]
+# for i in range(len(lip)):
+#         if(lip[i]<0):
+#           lip[i]=0
+        
+# print(lip) 
+
+# # 1. basic structure
+# # [kya chahiye  for item in list]
+# num=[1,2,3,4]
+# res=[]
+# res=[x*2 for x in num]
+# print(res)
+
+# # 2. sirf filter krna-> sirf if bina else ke
+# # jb kuch elements lo select krna ho or baki ko chhod dena ho tb if last me lgta h
+# #[kya chahiye  for item in list  if condition]
+# num1=[1,2,3,4,5,6]
+# evens=[]
+# odds=[]
+# evens=[u  for u in num1 if u%2==0]
+# odds=[p  for p in num1 if p%2!=0]
+# print(evens,"\n",odds)
+
+# # 3. value badalna -> jb if ke sath else bhi ho
+# # jb hr element ko rkhna h lekin condition ke hisab se value badalni ho
+# lite=[-4, -5, 6, 9, 1, 4, -3]
+# k=[0 if j>0 else j+5 for j in lite]
+# print(k)
+
+# words=["hello","python","apnacollege"]
+# words=[kl.upper()   for kl in words]
+# print(words)
+
+
+
+
+
+
+
 
 
 
